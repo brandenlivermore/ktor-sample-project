595f05717d9299cdc8566e71f294e75dcc21274f
Branden Livermore <branden.livermore@gmail.com>
2023-06-19 13:13:33 -0700

    cleanup

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,18 +3,14 @@ import * as autoscaling from './autoscaling';
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets'; // Allows building the docker image and uploading to ECR
 import * as path from "path"; // Helper for working with file paths
 import * as cdk from 'aws-cdk-lib';
-import { aws_apprunner as apprunner, Duration } from 'aws-cdk-lib';
+import { aws_apprunner as apprunner } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import { TagStatus } from "aws-cdk-lib/aws-ecr";
 
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-        //If you are running on a Mac using the new M1 chip, please change `../SampleApp` to `../../SampleApp`.
         const imageAsset = new DockerImageAsset(this, 'ImageAssets', {
           directory: path.join(__dirname, '../../Docker'),
         });
@@ -27,43 +23,9 @@ export class InfraStack extends cdk.Stack {
           {
             assumedBy: new iam.ServicePrincipal("build.apprunner.amazonaws.com"),
             description: `${this.stackName}-apprunner-role`,
-            // inlinePolicies: {
-            //   "kotlintest-apprunner-policy": new iam.PolicyDocument({
-            //     statements: [
-            //       new iam.PolicyStatement({
-            //         effect: iam.Effect.ALLOW,
-            //         actions: ["ecr:GetAuthorizationToken"],
-            //         resources: ["*"],
-            //       }),
-            //       new iam.PolicyStatement({
-            //         effect: iam.Effect.ALLOW,
-            //         actions: [
-            //           "ecr:BatchCheckLayerAvailability",
-            //           "ecr:GetDownloadUrlForLayer",
-            //           "ecr:GetRepositoryPolicy",
-            //           "ecr:DescribeRepositories",
-            //           "ecr:ListImages",
-            //           "ecr:DescribeImages",
-            //           "ecr:BatchGetImage",
-            //           "ecr:GetLifecyclePolicy",
-            //           "ecr:GetLifecyclePolicyPreview",
-            //           "ecr:ListTagsForResource",
-            //           "ecr:DescribeImageScanFindings",
-            //         ],
-            //         // resources: [
-            //         //   repository.repositoryArn
-            //         // ],
-            //       }),
-            //     ],
-            //   }),
-            // },
           }
         );
-
         appRunnerRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSAppRunnerServicePolicyForECRAccess"));
-
-        // Push the Docker image to the ECR repository
-        //imageAsset.repository = repository;
 
         const cfnService = new apprunner.CfnService(this, 'Service', {
           sourceConfiguration: {
